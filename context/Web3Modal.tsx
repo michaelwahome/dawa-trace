b48bdd50dffebe7e250469f0db916807bbf8d6fc
michaelwahome <michaelwahome1@gmail.com>
2024-01-06 03:25:56 +0300

    Added web3modal.

---
+++ context/Web3Modal.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { createWeb3Modal, defaultConfig } from '@web3modal/ethers/react'
+
+// 1. Get projectId at https://cloud.walletconnect.com
+const projectId = 'bad622f93f8446d277bded6bad26f09e'
+
+// 2. Set chains
+const mainnet = {
+  chainId: 1,
+  name: 'Ethereum',
+  currency: 'ETH',
+  explorerUrl: 'https://etherscan.io',
+  rpcUrl: 'https://cloudflare-eth.com'
+}
+
+// 3. Create modal
+const metadata = {
+  name: 'My Website',
+  description: 'My Website description',
+  url: 'https://mywebsite.com',
+  icons: ['https://avatars.mywebsite.com/']
+}
+
+createWeb3Modal({
+  ethersConfig: defaultConfig({ metadata }),
+  chains: [mainnet],
+  projectId
+})
+
+export function Web3ModalProvider({ children }: {children: any}) {
+  return children;
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+const projectId = process.env.PROJECT_ID
+const privateKey = process.env.PRIVATE_KEY
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks:{
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    }
+  },
   solidity: "0.8.19",
 };
