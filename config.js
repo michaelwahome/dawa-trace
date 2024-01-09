27d63ccab554b0d2920929ca444b452e48022465
michaelwahome <michaelwahome1@gmail.com>
2024-01-10 00:29:42 +0300

    Metadata and updated config.js (smart contract address)

--- config.js
+++ config.js
@@ -1,1 +1,3 @@
-export const pharmaceuticalAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+// export const pharmaceuticalAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // Localhost
+export const pharmaceuticalAddress = "0x2c2f97fe02697D2e267a8376eC961052cC02fdc0"; // Mumbai Old
+// export const pharmaceuticalAddress = "0xE95416A9C202b08171cDd2769EEb5C3beE87C01B"; // Mumbai
